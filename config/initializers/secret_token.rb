8841bdb16065ee7894dbf6d9782737293406773d
ricardo <ricardocruz.a89@gmail.com>
2012-07-20 18:52:59 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PpLanding::Application.config.secret_token = '990a462f2a78a174050be542d72444bae9e038f1e33db9bb2adf4c3f12dfb64be3efd0d5eb46dd72084647a4c5b34caee77cca64f1a67439670098b5e503ed08'
